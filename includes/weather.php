368261aa76b0b37bd4bdb7fdf7b2d7fe6552ed3f
Ben <lumezious@googlemail.com>
2013-10-17 16:46:03 +0100

    Updated weather.php to show weather info
    Updated weather.php to show weather info retrieved from the API. Added
    some css to style.css to format it nicely.

--- includes/weather.php
+++ includes/weather.php
@@ -2,8 +2,22 @@
 	$data = json_decode(file_get_contents('http://comp2203.ecs.soton.ac.uk/labs/lab3/api/location.php?location=3772'));
 ?>
 
-
-
 <div id="weather">
-	<?php print_r($data); ?>
+	<h3><?php print_r($data->name . ', ' . $data->country); ?></h3>
+	<p><?php print_r('Lat: ' . $data->position->lat . ', Long: ' . $data->position->long); ?></p>
+	<p><?php print_r('Continent: ' . $data->continent); ?></p>
+	
+	<h3>Current Weather</h3>
+	
+	<p><?php print_r('Time: ' . $data->time); ?></p>
+	<p><?php print_r($data->weather->weatherType); ?></p>
+	<p><?php print_r('Temp: ' . $data->weather->temperature); ?>&deg;C</p>
+	<p><?php print_r('Pressure: ' . $data->weather->pressure . 'mb');?></p>
+	<p><?php print_r('Visibility: ' . $data->weather->visibility / 1000 . 'km'); ?></p>
+	
+	<h5>Wind</h5>
+	
+	<p><?php print_r('Direction: ' . $data->weather->wind->compass . ', ' . $data->weather->wind->angle); ?>&deg;</p>
+	<p><?php print_r('Speed: ' . $data->weather->wind->speed . 'kph'); ?></p>
+	<pre><?php print_r($data); ?></pre>
 </div>
